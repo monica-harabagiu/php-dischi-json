d9b05011038daa97092f93843c8e825d0d95a0ef
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-02-28 14:57:50 +0100

    app style

--- index.php
+++ index.php
@@ -1,17 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi JSON</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
 
 </head>
+
 <body>
 
     <div id="app">
-        <h1>{{message}}</h1>
+        <header class="py-3 ">
+            <div class="container">
+                <figure class="m-0">
+                    <img src="./assets/img/Spotify_logo.png" alt="Spotify Logo">
+                </figure>
+            </div>
+        </header>
+
+        <main class="py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="cards d-flex">
+                        <div class="card py-4 px-5 text-center d-flex flex-column gap-2">
+                            <figure>
+                                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                            </figure>
+                            <h5 class="fw-bold ">New Jersey</h5>
+                            <h6>Bon Jovi</h6>
+                            <h5 class="fw-bold">1988</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 
 
@@ -20,8 +48,11 @@
 
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <script src="./js/main.js"></script>
-    
+
 </body>
+
 </html>
